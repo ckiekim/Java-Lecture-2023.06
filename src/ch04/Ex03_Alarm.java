685f0fbef67d448a08fceb2e51289d147b48355c
Cheolkie Kim <ckiekim@naver.com>
2023-06-22 09:35:13 +0900

    Ch04. 조건문 연습문제

---
+++ src/ch04/Ex03_Alarm.java
@@ -0,0 +1,24 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Ex03_Alarm {
+
+	public static void main(String[] args) {
+		int hour, minute, newHour, newMin;
+		Scanner scan = new Scanner(System.in);
+		hour = scan.nextInt();
+		minute = scan.nextInt();
+		scan.close();
+		
+		if (minute >= 45) {
+			newHour = hour;
+			newMin = minute - 45;
+		} else {
+			newHour = (hour + 24 - 1) % 24;
+			newMin = minute + 60 - 45;
+		}
+		System.out.printf("%02d:%02d", newHour, newMin);
+	}
+
+}
