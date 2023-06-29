fc15a329ce76c50b9bd436286535f4eeca2898a0
Cheolkie Kim <ckiekim@naver.com>
2023-06-29 12:47:59 +0900

    Ch13. Collections

--- src/ch13_collection/sec01_list/Ex04_Performance.java
+++ src/ch13_collection/sec01_list/Ex04_Performance.java
@@ -25,14 +25,14 @@ public class Ex04_Performance {
 		System.out.println("LinkedList 소요시간: " + (endTime - startTime) + " ns");
 		System.out.println("=====================================");
 		
-		// ArrayList 뒤에 100000회 추가하는 경우 - 13.7 ms
+		// ArrayList 뒤에 100000회 추가하는 경우 - 11.5 ms
 		startTime = System.nanoTime();
 		for (int i=0; i<100000; i++)
 			al.add(String.valueOf(i));
 		endTime = System.nanoTime();
 		System.out.println("ArrayList 소요시간: " + (endTime - startTime) + " ns");
 		
-		// LinkedList 뒤에 100000회 추가하는 경우 - 2.8 ms
+		// LinkedList 뒤에 100000회 추가하는 경우 - 24.7 ms
 		startTime = System.nanoTime();
 		for (int i=0; i<100000; i++)
 			ll.add(String.valueOf(i));
